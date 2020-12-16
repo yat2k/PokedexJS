51bf075e7ef4bd8d1c141d2252ab74e19cb64bea
Yathin-Rao <kingyatrulez@gmail.com>
2020-12-17 04:24:51 +0530

    updated

--- dex.js
+++ dex.js
@@ -17,3 +17,16 @@ const colors = {
 	normal: '#F5F5F5'
 };
 const main_types = Object.keys(colors);
+
+const fetchPokemons = async () => {
+	for (let i = 1; i <= pokemons_number; i++) {
+		await getPokemon(i);
+	}
+};
+
+const getPokemon = async id => {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const res = await fetch(url);
+	const pokemon = await res.json();
+	createPokemonCard(pokemon);
+};
