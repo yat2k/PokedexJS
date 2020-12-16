586022fda016109bae940b0d99f5f89c9574f25d
Yathin-Rao <kingyatrulez@gmail.com>
2020-12-17 03:49:00 +0530

    1 gen pokes

---
+++ dex.js
@@ -0,0 +1,19 @@
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 150;
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+const main_types = Object.keys(colors);
